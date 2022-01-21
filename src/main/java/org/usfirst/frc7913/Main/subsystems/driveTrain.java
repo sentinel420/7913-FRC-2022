32bb775df694f1a3eddcc3f4fbe181f31823ac5c
Dillon.Therrien <dillon.therrien@fnst.com>
2022-01-21 14:31:24 -0500

    Added comments and fixed formatting

--- src/main/java/org/usfirst/frc7913/Main/subsystems/driveTrain.java
+++ src/main/java/org/usfirst/frc7913/Main/subsystems/driveTrain.java
@@ -1,4 +1,5 @@
 package org.usfirst.frc7913.Main.subsystems;
+
 import org.usfirst.frc7913.Main.Robot;
 import org.usfirst.frc7913.Main.commands.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -17,25 +18,39 @@ public class driveTrain extends Subsystem {
     private DifferentialDrive driveTrain;
 
     public driveTrain() {
-        //Left motors
+        // Left motors looking from the back of the robot
+        // Front-Left motor
         leftLead = new PWMVictorSPX(0);
-        addChild("leftLead",leftLead);
+        addChild("leftLead", leftLead);
         leftLead.setInverted(false);
+        // Rear-Left motor
         leftFollow = new PWMVictorSPX(2);
-        addChild("leftFollow",leftFollow);
+        addChild("leftFollow", leftFollow);
         leftFollow.setInverted(false);
+
+        // Assigning left motors to the same controller group
         leftSide = new MotorControllerGroup(leftLead, leftFollow);
 
-        //Right motors
+        // Right motors looking from the back of the robot
+        // Front-Right motor
         rightLead = new PWMVictorSPX(1);
-        addChild("rightLead",rightLead);
+        addChild("rightLead", rightLead);
         rightLead.setInverted(false);
+
+        // Rear-Right motor
         rightFollow = new PWMVictorSPX(3);
-        addChild("rightFollow",rightFollow);
+        addChild("rightFollow", rightFollow);
         rightFollow.setInverted(false);
+
+        // Assigning right motors to the same controller group
         rightSide = new MotorControllerGroup(rightLead, rightFollow);
-        
+
+        // Using differential drive which is similar to a tank or skid steer
+        // If using mechanum drive, this must be changed to creating a MecanumDrive
+        // object instead
         driveTrain = new DifferentialDrive(leftSide, rightSide);
+
+        // Drivetrain config
         driveTrain.setSafetyEnabled(true);
         driveTrain.setExpiration(0.1);
         driveTrain.setMaxOutput(1.0);
@@ -48,7 +63,9 @@ public class driveTrain extends Subsystem {
 
     @Override
     public void periodic() {
-        //Line that sets speed of motors
+        // Gets X & Y axes from the joystick controller
+        // Arcade drive combines the two motor controller groups, first arg is forward/reverse, second is rotation.
+        // Unlike tank drive which has the two motor controller groups separate where the first arg is forward/reverse for the left side and the second is the same for the right
         driveTrain.arcadeDrive(Robot.oi.joystickx30.getY(), Robot.oi.joystickx30.getX(), true);
     }
 }
